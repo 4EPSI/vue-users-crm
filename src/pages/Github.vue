4c10de88e3fccea5aeb2638b3fa4eab698013415
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-02-13 10:39:32 +0200

    sort

--- src/pages/Github.vue
+++ src/pages/Github.vue
@@ -19,8 +19,17 @@
         <div v-if="repos || loading" class="wrapper__repos">
   
           <preloader v-if="loading" :width="90" :height="90" />
+
+          <div class="repos-sort">
+            <span @click="sort('name')">
+              Name {{ currentSort === 'name' ? (currentSortDir === 'asc' ? '▲' : '▼') : '' }}
+            </span>
+            <span @click="sort('stargazers_count')">
+              Stars {{ currentSort === 'stargazers_count' ? (currentSortDir === 'asc' ? '▲' : '▼') : '' }}
+            </span>
+          </div>
   
-          <div v-for="repo in repos" :key="repo.id" class="repos-item">
+          <div v-for="repo in sortedRepos" :key="repo.id" class="repos-item">
             <div class="repos-info">
               <a :href="repo.html_url" target="_blank" class="link">{{ repo.name }}</a>
               <span>{{ repo.stargazers_count }} ⭐</span>
@@ -46,12 +55,34 @@ export default {
       search: '',
       loading: false,
       error: null,
-      repos: null
+      repos: null,
+      currentSort: 'name',
+      currentSortDir: 'asc'
     }
   },
   // mounted() {
   //   this.getRepos()
   // },
+  computed: {
+    sortedRepos() {
+      if (!this.repos) return [];
+      
+      return [...this.repos].sort((a, b) => {
+        let modifier = this.currentSortDir === 'asc' ? 1 : -1;
+        let aValue = a[this.currentSort];
+        let bValue = b[this.currentSort];
+
+        if (this.currentSort === 'name') {
+          aValue = aValue.toLowerCase();
+          bValue = bValue.toLowerCase();
+        }
+
+        if (aValue < bValue) return -1 * modifier;
+        if (aValue > bValue) return 1 * modifier;
+        return 0;
+      });
+    }
+  },
   methods: {
     async getRepos() {
       // console.log(`get user ${this.search} repos`)
@@ -70,6 +101,14 @@ export default {
         this.error = 'Can`t find this user'
       } finally {
         this.loading = false
+      }
+    },
+    sort(sort) {
+      if(sort === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
+      } else {
+        this.currentSort = sort
+        this.currentSortDir = 'asc'
       }
     }
   }
@@ -103,5 +142,12 @@ button {
 }
 :deep(.preloader) {
   display: flex;
+}
+.repos-sort {
+  margin-bottom: 20px;
+  font-weight: 700;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
 }
 </style>
